b65cf28759a7700e7f98ee84986583b975fc693a
7TIN <prasadgujarathi619@gmail.com>
2026-03-11 01:31:41 +0530

    feat: updated hono server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,38 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { searchDocs } from "./services/search.ts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Docs Search API running");
+});
 
-export default app
-  
+app.get("/search", async (c) => {
+  const query = c.req.query("q");
+  const page = Number(c.req.query("page") || 1);
+  const perPage = Number(c.req.query("per_page") || 10);
+
+  if (!query) {
+    return c.json({ error: "Missing query parameter 'q'" }, 400);
+  }
+
+  try {
+    const result = await searchDocs({
+      query,
+      page,
+      perPage,
+    });
+
+    return c.json({
+      query,
+      found: result.found,
+      page: result.page,
+      results: result.hits,
+    });
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: "Search failed" }, 500);
+  }
+});
+
+export default app;
--- apps/api/src/services/search.ts
+++ apps/api/src/services/search.ts
@@ -1,15 +1,54 @@
 import { client } from "../client/typesenseClient.ts";
 
-async function search() {
+type SearchOptions = {
+  query: string;
+  page?: number;
+  perPage?: number;
+};
+
+// async function search() {
+//   const result = await client
+//     .collections("docs")
+//     .documents()
+//     .search({
+//       q: "routing",
+//       query_by: "heading,content,code",
+//     });
+
+//   console.log(result.hits);
+// }
+
+// search();
+
+
+export async function searchDocs({ query, page = 1, perPage = 10 }: SearchOptions) {
   const result = await client
     .collections("docs")
     .documents()
     .search({
-      q: "routing",
+      q: query,
+
+      // what fields to search
       query_by: "heading,content,code",
+
+      // importance ranking
+      query_by_weights: "10,5,2",
+
+      // allow partial matching
+      prefix: true,
+
+      // typo tolerance
+      num_typos: 2,
+
+      // highlight matches
+      highlight_fields: "heading,content,code",
+
+      // limit returned fields
+      include_fields: "title,heading,content,url",
+
+      page,
+      per_page: perPage,
     });
 
-  console.log(result.hits);
+  return result;
 }
-
-search();
